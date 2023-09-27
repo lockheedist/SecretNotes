d61f3de7a164ff9bc9e4fee9e2e6a8a6acdf161d
Burak Keskin <burakkeskinn1@icloud.com>
2023-09-27 17:12:00 +0300

    Encryption Added. Final Commit

--- main.py
+++ main.py
@@ -1,6 +1,53 @@
 from tkinter import *
 from PIL import ImageTk, Image
-from cryptography.fernet import Fernet
+from tkinter import messagebox
+import base64
+
+def clearstrings():
+    keyentry.delete(0,END)
+    titleentry.delete(0,END)
+    textbox.delete("1.0",END)
+
+
+skey=12
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+def save_and_encrypt():
+    title=titleentry.get()
+    message=textbox.get("1.0","end-1c")
+    master_secret=keyentry.get()
+    if len(title) == 0 or len(message) == 0 or len(master_secret)==0:
+        messagebox.showwarning("Input Error","Please provide correct inputs!")
+    else:
+        #encryptionlater
+        with open("keys.txt","a") as datas:
+            datas.write(f"\n{title}\n{encode(master_secret,message)}")
+        clearstrings()
+
+def decrypt():
+    message = textbox.get("1.0", "end-1c")
+    master_secret=keyentry.get()
+    decrypted_message=decode(master_secret,message)
+    clearstrings()
+    #textbox.insert("1.0",decrypted_message)
+    messagebox.showinfo("Decrypted Note",f"{decrypted_message}")
+
+
 
 wn = Tk()
 wn.title("Secret Notes @lockheedist")
@@ -30,27 +77,22 @@ keyentry= Entry()
 keyentry.pack()
 
 
+
+
 #Save&Encrypt Button
-saveenrcypt= Button(text="Save&Encrypt")
+saveenrcypt= Button(text="Save&Encrypt",command=save_and_encrypt)
+saveenrcypt.config()
 saveenrcypt.pack()
 
 
 #Decrypt button
-decrypt= Button(text="Decryption")
+decrypt= Button(text="Decryption",command=decrypt)
 decrypt.pack()
 
-titlegetintro = titleentry.get()
-textandtitlegetintro= str(textbox.get("1.0",'end-1c'))+" "+str(titleentry.get())
-message = "hi"
-def keysappend():
-    with open("keys.txt","at") as keys:
-        keys.write(f"\ntest")
 
-def generatedecrypt():
-    message=str(textbox.get("1.0",'end-1c'))+" "+str(titleentry.get())
-    key = Fernet.generate_key()
-    fernet = Fernet(key)
-    encMessage = fernet.encrypt(message.encode())
+
+
+
 
 
 
